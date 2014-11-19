fefde28f621896d1f8300651ff217ad29c9efa43
bw6 <bw6@bw6.fastralee.com>
2014-11-19 15:12:27 +0000

    Room abstraction

--- src/main/java/Hotel/database/rows/Room.java
+++ src/main/java/Hotel/database/rows/Room.java
@@ -3,10 +3,63 @@ package Hotel.database.rows;
 import Hotel.database.Row;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Room implements Row {
+    private Integer id;
+    private Integer roomType;
+    private Integer roomCost;
+    private Boolean status;
+
     @Override
     public void setData(ResultSet resultSet) {
+        try {
+            this.id = resultSet.getInt("id");
+            this.roomType = resultSet.getInt("roomType");
+            this.roomCost = resultSet.getInt("roomCost");
+            this.status = resultSet.getBoolean("status");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    // region Getters and setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(Integer roomType) {
+        this.roomType = roomType;
+    }
+
+    public Integer getRoomCost() {
+        return roomCost;
+    }
+
+    public void setRoomCost(Integer roomCost) {
+        this.roomCost = roomCost;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+
+    // endregion
+
+    public String toString() {
+        return String.format(
+                "ID: %s | roomType: %s | roomCost: %s | status: %s",
+                this.id, this.roomType, this.roomCost, this.status
+        );
     }
 }
