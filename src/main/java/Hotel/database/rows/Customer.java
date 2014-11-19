997d9a6fbdda744d81d8ea5fdf51ae6a79a4ed20
bw7 <bw7@bw7.fastralee.com>
2014-11-19 15:20:06 +0000

    customer abstraction

--- src/main/java/Hotel/database/rows/Customer.java
+++ src/main/java/Hotel/database/rows/Customer.java
@@ -3,10 +3,54 @@ package Hotel.database.rows;
 import Hotel.database.Row;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Customer implements Row {
+	
+	private String fName ;
+	private String lName;
+	private Integer cid;
+	
     @Override
     public void setData(ResultSet resultSet) {
-
+    	
+    	try {
+			this.cid = resultSet.getInt(cid);
+	
+			this.fName = resultSet.getString(fName);
+			this.lName = resultSet.getString(lName);
+    	} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+	
+	public String toString(){
+		return String.format("CID : %d | firstName : %s | lastName : %s ",this.cid,this.fName,this.lName);
+	}
 }
